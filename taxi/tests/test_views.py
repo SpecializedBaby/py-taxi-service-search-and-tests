a18275edb08695f8d35ccdaa1eb9e7dc68fcf5cd
Dede <specialized8393@gmail.com>
2024-05-17 20:17:42 +0100

    Implemeted PrivetCarListViewTest

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.models import Manufacturer
+from taxi.models import Manufacturer, Car
 
 MANUFACTURER_LIST_URL = "taxi:manufacturer-list"
 CAR_LIST_URL = "taxi:car-list"
@@ -34,7 +34,7 @@ class PrivetManufacturerListViewTest(TestCase):
         )
         self.client.force_login(self.user)
 
-    def test_response_manufacturers(self):
+    def test_response_template(self):
         response = self.client.get(reverse(MANUFACTURER_LIST_URL))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "taxi/manufacturer_list.html")
@@ -70,13 +70,74 @@ class PrivetManufacturerListViewTest(TestCase):
         self.assertEqual(list(queryset), list(filtered_queryset))
 
 
-class PublicDriverListViewTest(TestCase):
-    def test_login_required(self):
-        res = self.client.get(CAR_LIST_URL)
-        self.assertNotEqual(res.status_code, 200)
-
-
 class PublicCarListViewTest(TestCase):
     def test_login_required(self):
-        res = self.client.get(DRIVER_LIST_URL)
+        res = self.client.get(reverse(DRIVER_LIST_URL))
         self.assertNotEqual(res.status_code, 200)
+
+
+class PrivetCarListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        number_of_manufacturers = 8
+
+        for car_id in range(number_of_manufacturers):
+            Car.objects.create(
+                model=f"Car model {car_id}",
+                manufacturer=Manufacturer.objects.create(
+                    name=f"Manufacturer name {car_id}",
+                    country=f"Manufacturer country {car_id}",
+                ),
+            )
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test123",
+        )
+        self.client.force_login(self.user)
+
+    def test_response_template(self):
+        response = self.client.get(reverse(CAR_LIST_URL))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/car_list.html")
+
+    def test_pagination_is_5(self):
+        response = self.client.get(reverse(CAR_LIST_URL))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertTrue(len(response.context["car_list"]) == 5)
+        self.assertEqual(list(response.context["car_list"]), list(Car.objects.all()[:5]))
+
+    def test_list_all_cars(self):
+        response = self.client.get(reverse(CAR_LIST_URL) + "?page=2")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertTrue(len(response.context["car_list"]) == 3)
+        self.assertEqual(list(response.context["car_list"]), list(Car.objects.all()[5:]))
+
+    def test_view_search_form_in_context(self):
+        response = self.client.get(reverse(CAR_LIST_URL))
+        self.assertTrue("search_form" in response.context)
+
+    def test_search_form_on_queryset(self):
+        model = "1"
+        response = self.client.get(reverse(CAR_LIST_URL), {"model": model})
+        self.assertEqual(response.status_code, 200)
+
+        queryset = response.context["car_list"]
+        filtered_queryset = Car.objects.filter(model__icontains=model)
+
+        self.assertEqual(list(queryset), list(filtered_queryset))
+
+
+class PublicDriverListViewTest(TestCase):
+    def test_login_required(self):
+        res = self.client.get(reverse(CAR_LIST_URL))
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PrivetDriverListViewTest(TestCase):
+    pass
