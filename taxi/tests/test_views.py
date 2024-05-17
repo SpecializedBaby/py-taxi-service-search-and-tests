af3694f91ab269cb73506012bcdabef595a53990
Dede <specialized8393@gmail.com>
2024-05-17 19:21:12 +0100

    Implemented public tests for Drivers, Manufacturers and Cars views and tests with user login for Manufacturer views

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -1,2 +1,82 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
+from taxi.models import Manufacturer
+
+MANUFACTURER_LIST_URL = "taxi:manufacturer-list"
+CAR_LIST_URL = "taxi:car-list"
+DRIVER_LIST_URL = "taxi:driver-list"
+
+
+class PublicManufacturerListViewTest(TestCase):
+    def test_login_required(self):
+        res = self.client.get(reverse(MANUFACTURER_LIST_URL))
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PrivetManufacturerListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 13 manufacturers for pagination tests
+        number_of_manufacturers = 8
+
+        for manufacturer_id in range(number_of_manufacturers):
+            Manufacturer.objects.create(
+                name=f"Name {manufacturer_id}",
+                country=f"Country {manufacturer_id}",
+            )
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test123",
+        )
+        self.client.force_login(self.user)
+
+    def test_response_manufacturers(self):
+        response = self.client.get(reverse(MANUFACTURER_LIST_URL))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/manufacturer_list.html")
+
+    def test_pagination_is_5(self):
+        response = self.client.get(reverse(MANUFACTURER_LIST_URL))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertEqual(len(response.context["manufacturer_list"]), 5)
+        self.assertEqual(list(response.context["manufacturer_list"]), list(Manufacturer.objects.all()[:5]))
+
+    def test_list_all_manufacturers(self):
+        response = self.client.get(reverse(MANUFACTURER_LIST_URL) + "?page=2")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"] is True)
+        self.assertEqual(len(response.context["manufacturer_list"]), 3)
+        self.assertEqual(list(response.context["manufacturer_list"]), list(Manufacturer.objects.all()[5:]))
+
+    def test_view_search_form_in_context(self):
+        response = self.client.get(reverse(MANUFACTURER_LIST_URL))
+        self.assertTrue("search_form" in response.context)
+
+    def test_search_form_on_queryset(self):
+        search_query = "1"
+        response = self.client.get(reverse(MANUFACTURER_LIST_URL), {"name": search_query})
+        self.assertEqual(response.status_code, 200)
+
+        queryset = response.context["manufacturer_list"]
+        filtered_queryset = Manufacturer.objects.filter(name__icontains=search_query)
+
+        self.assertEqual(list(queryset), list(filtered_queryset))
+
+
+class PublicDriverListViewTest(TestCase):
+    def test_login_required(self):
+        res = self.client.get(CAR_LIST_URL)
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PublicCarListViewTest(TestCase):
+    def test_login_required(self):
+        res = self.client.get(DRIVER_LIST_URL)
+        self.assertNotEqual(res.status_code, 200)
